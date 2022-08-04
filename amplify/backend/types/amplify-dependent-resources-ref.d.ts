c2b94c3d24399165772598b7bb7cb7053c1926e6
Dhrusha prajapati <dhrushaprajapati@Dhrushas-MacBook-Pro.local>
2022-08-04 16:24:53 +0530

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,43 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "testamplifyd": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "testamplifyd1": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "apitest": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "auth": {
+        "test7ec3e94b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "cognitodemoGroupRole": "string"
+        }
+    },
+    "api": {
+        "apitestwithfun": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
